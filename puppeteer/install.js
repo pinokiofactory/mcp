534dbb803f4b11f9ab514a65b34426dba3e8521c
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-21 10:03:23 -0500

    update

--- puppeteer/install.js
+++ puppeteer/install.js
@@ -1,13 +1,16 @@
 module.exports = async (kernel) => {
-  const config =  {
-    darwin: kernel.path(kernel.envs.HOME, "Library/Application Support/Claude/claude_desktop_config.json"),
-    win32: kernel.path(kernel.envs.APPDATA, 'Claude/claude_desktop_config.json')
+  let config
+  if (kernel.platform === "darwin") {
+    config = kernel.path(kernel.envs.HOME, "Library/Application Support/Claude/claude_desktop_config.json"),
+  } else if (kernel.platform === "win32") {
+    config = kernel.path(kernel.envs.APPDATA, 'Claude/claude_desktop_config.json')
   }
+  console.log({ config })
   return {
     run: [{
       method: "json.set",
       params: {
-        [config[kernel.platform]]: {
+        [config]: {
           "mcpServers.puppeteer": {
             "command": "npx",
             "args": [
--- puppeteer/uninstall.js
+++ puppeteer/uninstall.js
@@ -1,15 +1,18 @@
 module.exports = async (kernel) => {
-  const config =  {
-    darwin: kernel.path(kernel.envs.HOME, "Library/Application Support/Claude/claude_desktop_config.json"),
-    win32: kernel.path(kernel.envs.APPDATA, 'Claude/claude_desktop_config.json')
+  let config
+  if (kernel.platform === "darwin") {
+    config = kernel.path(kernel.envs.HOME, "Library/Application Support/Claude/claude_desktop_config.json"),
+  } else if (kernel.platform === "win32") {
+    config = kernel.path(kernel.envs.APPDATA, 'Claude/claude_desktop_config.json')
   }
+  console.log({ config })
   return {
     run: async (kernel) => {
       return [
         {
           method: "json.rm",
           params: {
-            [config[kernel.platform]]: ["mcpServers.puppeteer"]
+            [config]: ["mcpServers.puppeteer"]
           }
         },
       ]
