697094c3738c36d4e037d5edd845228e654c8f2b
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-21 09:41:36 -0500

    update

--- pinokio.js
+++ pinokio.js
@@ -1,41 +1,43 @@
 const path = require('path')
-module.exports = async (kernel) => {
-  let p
-  if (kernel.platform === "darwin") {
-    p = `${path.resolve(kernel.envs.HOME, "Library/Application Support/Claude/claude_desktop_config.json")}`
-  } else if (kernel.platform === "win32") {
-    p = `${path.resolve(kernel.envs.APPDATA, 'Claude/claude_desktop_config.json')}`
-  }
+module.exports = {
   return {
     version: "2.2",
     title: "MCP",
     description: "",
     icon: "icon.png",
-    menu: [
-      "fetch",
-      "github",
-      "memory",
-      "filesystem",
-      "puppeteer",
-      "brave-search",
-      "google-maps",
-      "sqlite",
-      "everart",
-    ].map((x) => {
-      return {
-        text: x,
-        menu: [
-          { text: "Install", href: `${x}/install.js` },
-          { text: "Uninstall", href: `${x}/uninstall.js` }
-        ]
+    menu: async (kernel) => {
+      let p
+      if (kernel.platform === "darwin") {
+        p = path.resolve(kernel.envs.HOME, "Library/Application Support/Claude/claude_desktop_config.json")
+      } else if (kernel.platform === "win32") {
+        p = path.resolve(kernel.envs.APPDATA, 'Claude/claude_desktop_config.json')
       }
-    }).concat([{
-      text: "Update",
-      href: "update.json"
-    }, {
-      text: "Open MCP Config File",
-      href: p,
-      fs: true
-    }])
+      return [
+        "fetch",
+        "github",
+        "memory",
+        "filesystem",
+        "puppeteer",
+        "brave-search",
+        "google-maps",
+        "sqlite",
+        "everart",
+      ].map((x) => {
+        return {
+          text: x,
+          menu: [
+            { text: "Install", href: `${x}/install.js` },
+            { text: "Uninstall", href: `${x}/uninstall.js` }
+          ]
+        }
+      }).concat([{
+        text: "Update",
+        href: "update.json"
+      }, {
+        text: "Open MCP Config File",
+        href: p,
+        fs: true
+      }])
+    }
   }
 }
