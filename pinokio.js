d969c8687c1ba51a1f0d158b54bbfd0e03242b06
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-21 09:43:32 -0500

    update

--- pinokio.js
+++ pinokio.js
@@ -1,43 +1,41 @@
 const path = require('path')
 module.exports = {
-  return {
-    version: "3.0",
-    title: "MCP",
-    description: "",
-    icon: "icon.png",
-    menu: async (kernel) => {
-      let p
-      if (kernel.platform === "darwin") {
-        p = path.resolve(kernel.envs.HOME, "Library/Application Support/Claude/claude_desktop_config.json")
-      } else if (kernel.platform === "win32") {
-        p = path.resolve(kernel.envs.APPDATA, 'Claude/claude_desktop_config.json')
-      }
-      return [
-        "fetch",
-        "github",
-        "memory",
-        "filesystem",
-        "puppeteer",
-        "brave-search",
-        "google-maps",
-        "sqlite",
-        "everart",
-      ].map((x) => {
-        return {
-          text: x,
-          menu: [
-            { text: "Install", href: `${x}/install.js` },
-            { text: "Uninstall", href: `${x}/uninstall.js` }
-          ]
-        }
-      }).concat([{
-        text: "Update",
-        href: "update.json"
-      }, {
-        text: "Open MCP Config File",
-        href: p,
-        fs: true
-      }])
+  version: "3.0",
+  title: "MCP",
+  description: "",
+  icon: "icon.png",
+  menu: async (kernel) => {
+    let p
+    if (kernel.platform === "darwin") {
+      p = path.resolve(kernel.envs.HOME, "Library/Application Support/Claude/claude_desktop_config.json")
+    } else if (kernel.platform === "win32") {
+      p = path.resolve(kernel.envs.APPDATA, 'Claude/claude_desktop_config.json')
     }
+    return [
+      "fetch",
+      "github",
+      "memory",
+      "filesystem",
+      "puppeteer",
+      "brave-search",
+      "google-maps",
+      "sqlite",
+      "everart",
+    ].map((x) => {
+      return {
+        text: x,
+        menu: [
+          { text: "Install", href: `${x}/install.js` },
+          { text: "Uninstall", href: `${x}/uninstall.js` }
+        ]
+      }
+    }).concat([{
+      text: "Update",
+      href: "update.json"
+    }, {
+      text: "Open MCP Config File",
+      href: p,
+      fs: true
+    }])
   }
 }
